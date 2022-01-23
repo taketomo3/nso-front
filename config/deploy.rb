34fd5b70a795faa432cbadafa8680632f449c888
竹田智哉 <andalso316@gmail.com>
2022-01-23 12:26:12 +0900

    Update deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,29 @@
 lock "~> 3.16.0"
 
 set :application, "nso-front"
-set :repo_url, "git@github.com:taketomo3/nso-front.git"
+set :repo_url, "git@github.com:taketomo3/#{fetch(:application)}.git"
+
+namespace :deploy do
+  desc "Install dependencies"
+  task :install_dependencies do
+    on roles(:web) do
+      execute "npm install"
+    end
+  end
+
+  after :updated, :install_dependencies
+
+  desc "Restart application"
+  task :restart do
+    invoke "pm2:restart"
+  end
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# set :deploy_to, "/var/www/nso-front"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -33,7 +49,7 @@ set :repo_url, "git@github.com:taketomo3/nso-front.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,17 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "nso", user: "takeda", roles: %w{app}
+
+set :branch, :main
+set :deploy_to, "/srv/nso/front"
+
+set :ssh_options, {
+  port: 22,
+  forward_agent: true,
+  # ssh秘密鍵の場所
+  keys: ['~/.ssh/nso.key']
+}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
