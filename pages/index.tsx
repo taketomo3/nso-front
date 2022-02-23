93816a93977c494a5d7b2fbc657da4b01751266d
竹田智哉 <andalso316@gmail.com>
2022-02-23 12:45:19 +0900

    Setup prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div className='text-red-500'>
-      test updated
-    </div>
-  )
+  return <div className="text-red-500">test updated</div>
 }
 
 export default Home
